b4159ffee7f689ed11514512f8065678226f8493
kilian vi <vi.kilian@live.fr>
2020-10-09 14:00:36 +0200

    Ajoutez des fichiers projet.

---
+++ Projet_groupe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_groupe
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+            Console.WriteLine("hello world");
+        }
+    }
+}
